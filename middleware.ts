1224d487770c173f687a89449c3dc7d29d8609ce
6ixB <124480418+6ixB@users.noreply.github.com>
2024-08-18 20:56:02 +0700

    feat: add webhook api endpoint to sync Clerk events

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
   "/",
+  "/api/webhooks(.*)",
   "/auth/sign-in(.*)",
   "/auth/sign-up(.*)",
 ]);
