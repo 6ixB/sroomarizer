3506f060e4a7e5b87f86a8ca959d2b32f65b7e67
6ixB <124480418+6ixB@users.noreply.github.com>
2024-08-22 13:06:27 +0700

    fix: fix signup card clipping the dot pattern background mask

--- app/auth/sign-up/[[...sign-up]]/page.tsx
+++ app/auth/sign-up/[[...sign-up]]/page.tsx
@@ -6,7 +6,7 @@ import BlurFade from "@/components/magicui/blur-fade";
 export default function AuthSignUpPage() {
   return (
     <main className="flex min-h-screen justify-center">
-      <section className="bg-backgroundl relative flex h-[700px] w-full flex-col items-center gap-y-8 overflow-hidden rounded-l p-12">
+      <section className="bg-backgroundl relative flex h-[800px] w-full flex-col items-center gap-y-8 overflow-hidden rounded-l p-12">
         <div className="z-20 flex items-center justify-center">
           <BlurFade delay={0.75} inView>
             <SignUp />
@@ -14,7 +14,7 @@ export default function AuthSignUpPage() {
         </div>
         <DotPattern
           className={cn(
-            "[mask-image:radial-gradient(350px_circle_at_center,white,transparent)]",
+            "[mask-image:radial-gradient(400px_circle_at_center,white,transparent)]",
           )}
         />
       </section>
