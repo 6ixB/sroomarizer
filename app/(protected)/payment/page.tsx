115872f3a9d675010b7d04faf7d7af1f733b4a36
6ixB <124480418+6ixB@users.noreply.github.com>
2024-08-22 18:34:11 +0700

    fix: fix payment history is hidden at small to medium screens

--- app/(protected)/payment/page.tsx
+++ app/(protected)/payment/page.tsx
@@ -23,7 +23,7 @@ export default async function PaymentPage() {
 
   return (
     <ContentLayout title="Payment History">
-      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+      <div className="flex h-full flex-1 flex-col space-y-8 p-8">
         <div className="flex items-center justify-between space-y-2">
           <div>
             <h2 className="text-2xl font-bold tracking-tight">
