9971bf302332019521250cdfc359281d8a5aab25
6ixB <124480418+6ixB@users.noreply.github.com>
2024-08-22 13:04:52 +0700

    refactor: change footer text to be non-selectable

--- components/base/public/footer.tsx
+++ components/base/public/footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer className="mt-12 py-6 md:px-8 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-        <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+        <p className="select-none text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
           Built by&nbsp;
           <a
             target="_blank"
