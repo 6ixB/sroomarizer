aac680b423b853a2a6f31c962617d939c781b1bc
StefanieSheryl <85792580+StefanieSheryl@users.noreply.github.com>
2024-08-27 00:37:51 +0700

    refactor: adjusted payment table to suitable data type

--- components/pages/payment/data-table-row-actions.tsx
+++ components/pages/payment/data-table-row-actions.tsx
@@ -19,7 +19,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 import { labels } from "@/lib/data/data";
-import { taskSchema } from "@/lib/data/schema";
+import { exampleDataSchema } from "@/lib/data/example-data-schema";
 
 interface DataTableRowActionsProps<TData> {
   row: Row<TData>;
@@ -28,7 +28,7 @@ interface DataTableRowActionsProps<TData> {
 export function DataTableRowActions<TData>({
   row,
 }: DataTableRowActionsProps<TData>) {
-  const task = taskSchema.parse(row.original);
+  const task = exampleDataSchema.parse(row.original);
 
   return (
     <DropdownMenu>
@@ -49,7 +49,7 @@ export function DataTableRowActions<TData>({
         <DropdownMenuSub>
           <DropdownMenuSubTrigger>Labels</DropdownMenuSubTrigger>
           <DropdownMenuSubContent>
-            <DropdownMenuRadioGroup value={task.label}>
+            <DropdownMenuRadioGroup value={task.payment_method}>
               {labels.map((label) => (
                 <DropdownMenuRadioItem key={label.value} value={label.value}>
                   {label.label}
