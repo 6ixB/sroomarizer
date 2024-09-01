3a460b48df01094366e2c95af00ad8986cd5c614
StefanieSheryl <85792580+StefanieSheryl@users.noreply.github.com>
2024-09-01 15:06:44 +0700

    feat: added token amount in navbar

--- components/base/protected/navbar.tsx
+++ components/base/protected/navbar.tsx
@@ -1,12 +1,16 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { SheetMenu } from "@/components/base/protected/sheet-menu";
 import { ClerkLoaded, UserButton } from "@clerk/nextjs";
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 interface NavbarProps {
   title: string;
 }
 
 export function Navbar({ title }: NavbarProps) {
+
   return (
     <header className="sticky top-0 z-10 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:shadow-secondary">
       <div className="mx-4 flex h-14 items-center sm:mx-8">
@@ -15,6 +19,9 @@ export function Navbar({ title }: NavbarProps) {
           <h1 className="font-bold">{title}</h1>
         </div>
         <div className="flex flex-1 items-center justify-end space-x-2">
+        <Link href={`/tokens`}>
+          <Button variant="outline">🪙 100</Button>
+        </Link>
           <ModeToggle />
           <ClerkLoaded>
             <UserButton
